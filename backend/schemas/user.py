2e0d3ec21f2d9ff4a2d619ab21f9465e8db9b8d0
= <adrianramos2017@outlook.com>
2025-11-26 15:47:22 +0100

    User Schema created

---
+++ backend/schemas/user.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+from typing import Optional
+from pydantic import BaseModel
+
+class UserCreate(BaseModel):
+    username: str
+    email: str
+    password: str
+
+class UserUpdate(BaseModel):
+    username: Optional[str] = None
+    email: Optional[str] = None
+    password: Optional[str] = None
+    last_login: Optional[datetime] = None
+
+class UserResponse(BaseModel):
+    id: int
+    username: str
+    email: str
+    credits: float
+    created_at: datetime
+    
+    class Config():
+        orm_mode = True
